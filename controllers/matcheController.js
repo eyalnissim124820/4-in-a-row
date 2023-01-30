191be787076696dc6e409f7191c9388e3d102aa3
Gilad Shaul <giladshaul@hotmail.com>
2023-01-30 16:00:12 +0200

    Matches Routes

--- controllers/matcheController.js
+++ controllers/matcheController.js
@@ -4,7 +4,8 @@ const getUserLastScore = async (req, res) => {
   try {
     const userLastScore = await supabase
       .from("matches")
-      .where({ u1_Id: req.params.id } || { u2_Id: req.params.id })
+      .select("*")
+      .eq("winner", req.params.id)
       .first();
     res.send(userLastScore);
   } catch (err) {
@@ -12,11 +13,48 @@ const getUserLastScore = async (req, res) => {
   }
 };
 
+function filterConsecutive(array, column) {
+  return array.filter((row, index) => {
+    if (index === 0) return true;
+    return row[column] === array[index - 1][column] + 1;
+  });
+}
+
 const getUsersLongStrike = async (req, res) => {
   try {
     const longStrike = await supabase
       .from("matches")
-      .where({ id: req.params.id });
+      .select("*")
+      .eq("winner", req.params.id);
+    const newLongStrike = filterConsecutive(longStrike, "match_id");
+    return newLongStrike;
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const addMatche = async (req, res) => {
+  const { u1_id, u2_id, winner } = req.body;
+  try {
+    const newMatche = {
+      u1_id: u1_id,
+      us2_id: u2_id,
+      winner: winner,
+    };
+    const matcheAdded = await supabase.from("matches").insert(newMatche);
+    res.send({ ok: true });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const getUsersMatcheHistory = async (req, res) => {
+  try {
+    const usersHistory = await supabase
+      .from("matcues")
+      .select("*")
+      .eq("u1_id" || "u2_id", req.params.id);
+    res.send(usersHistory);
   } catch (err) {
     res.status(500).send(err);
   }
