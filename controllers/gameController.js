c6421396581e735d4cf3b329cca11f59e3824d63
Amir Ashlag <ashlag.amir@gmail.com>
2023-01-31 13:59:13 +0200

    o

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,6 +1,3 @@
-const {io} = require("socket.io-client")
-const socket = io("http://localhost:6000");
-
 function createRoom(req, res){
     const gameId = Math.floor(Math.random() * 9000) + 1000
     // console.log(gameId)
