bc5a46cb84310a8a2d5594618e9807c85888879e
Renzo Vargas <renzovargas172@gmail.com>
2023-01-31 20:21:33 +0200

    sockets added

--- server.js
+++ server.js
@@ -1,18 +1,66 @@
 const express = require("express");
-const cors = require("cors");
+const http = require("http");
 const app = express();
+const server = http.createServer(app);
 const usersRoute = require("./Routes/userRoutes");
 const matcheRoute = require("./Routes/matcheRoutes");
 const gameRoute = require("./Routes/gameRoute");
-
+const cors = require("cors");
+const { joinRoom } = require("./users");
+const { join } = require("path");
+const { message } = require("statuses");
+const { json } = require("body-parser");
 app.use(express.json());
 app.use(cors());
 
-app.use("/users", usersRoute);
-app.use("/matches", matcheRoute);
-app.use('/game',gameRoute)
-
-app.listen("8080", () => {
-  console.log("listening on port 8080");
+const sockeIO = require("socket.io")(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
 });
 
+let usersOnRoom = []
+sockeIO.on("connection", (socket) => {
+  console.log("connected", socket.id);
+
+  socket.on("joinRoom1", async(data) => {
+    await socket.join(data.roomId)
+    if(usersOnRoom[0] != data){
+      usersOnRoom.push(data)
+    }
+    const roomUsers = sockeIO.sockets.adapter.rooms.get(data.roomId);
+    // if(roomUsers.size === 2){
+    //   sockeIO.to(data.roomId).emit("usersInRoom", usersOnRoom);
+    // }  
+    console.log(roomUsers)
+  });
+  socket.on("joinRoom2", async(data) => {
+    
+    await socket.join(data.roomId)
+    if(usersOnRoom[0] !=data){
+      usersOnRoom.push(data)
+    }
+    const roomUsers = sockeIO.sockets.adapter.rooms.get(data.roomId);
+    if(roomUsers.size === 2){
+      sockeIO.to(data.roomId).emit("usersInRoom", usersOnRoom);
+      usersOnRoom = []
+    }  
+    console.log(roomUsers)
+  });
+
+  socket.on("update", (table, room) => {
+    socket.to(room).emit(table);
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+
+  });
+});
+
+app.use("/users", usersRoute);
+app.use("/matches", matcheRoute);
+app.use("/game", gameRoute);
+
+server.listen("8080", () => {
+  console.log("listening on port 8080");
+});
